a1ac564866043ff5e27fbac7a3c685e48568fe4e
quirogak <quirogakill@gmail.com>
2022-09-20 15:11:37 -0500

    Constructing book objects and storing them.
    The script takes the users input to create new Books and store these books inside of an Array.

--- script.js
+++ script.js
@@ -13,6 +13,30 @@ function Book(title,author,pages,read){
 
 }
 
+let myLibrary = []
+
+const addBookToLibrary = function()  {
+
+const title = prompt("Please put book name")
+const author = prompt("Please put author name")
+const pages = prompt("How many pages has the book")
+const read = prompt("did you read it already?")
+
+
+let newBook = new Book(title,author,pages,read)
+
+const addBook = myLibrary.push(newBook)
+
+return addBook
+
+}
+
+console.log(addBookToLibrary())
+
+console.log(myLibrary)
+
+/*
+
 const TheHobbit = new Book("The Hobbit "," J.R.R Tolkien"," 295 pages"," not read yet")
 
-console.log(TheHobbit.info())
+console.log(TheHobbit) */
